aaf4e32e477b355d98b877a41cdab2a69afcba3f
raziman <raziman@tutanota.com>
2022-10-24 10:53:08 +0800

    add invitation request

--- src/commands/Battle.ts
+++ src/commands/Battle.ts
@@ -1,6 +1,7 @@
+import { ButtonHandler } from "@jiman24/discordjs-button";
 import { random } from "@jiman24/discordjs-utils";
-import { Command } from "@jiman24/slash-commandment";
-import { CommandInteraction } from "discord.js";
+import { Command, CommandError } from "@jiman24/slash-commandment";
+import { CommandInteraction, EmbedBuilder } from "discord.js";
 import { Game } from "../structure/Game";
 import { Player } from "../structure/Player";
 
@@ -39,6 +40,24 @@ export default class extends Command {
 
       playerB.description = "Bot";
       playerB.imageUrl = user.displayAvatarURL();
+    }
+
+    const prompt = new EmbedBuilder()
+      .setColor("Random")
+      .setDescription(`${playerA.mention} invited ${playerB.mention} to a battle. Do you accept?`);
+    const confirmation = new ButtonHandler(i, [playerA.show(), prompt], user.id);
+
+    let inviteAccepted = false;
+
+    confirmation.addButton("Accept", () => { inviteAccepted = true });
+    confirmation.addButton("Reject", () => { inviteAccepted = false });
+
+    if (!playerB.isBot) {
+      await confirmation.run();
+    }
+
+    if (!inviteAccepted) {
+      throw new CommandError(`${playerB.mention} rejected the battle invitation`);
     }
 
     const game = new Game(i, playerA, playerB);
